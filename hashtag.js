ab50126144ae4f279609513626e12e5578b30fd3
Jungle <devex.soft@gmail.com>
2015-01-11 13:42:57 +0500

    initial commit

---
+++ hashtag.js
@@ -0,0 +1,42 @@
+$(function () {
+    var $container = $('#container');
+    var $symbol = $('#symbol');
+    var $hashtag = $('#hashtag');
+    var $tools = $('#tools');
+
+    function hashchange () {
+        var hash = window.location.hash;
+        var tag = hash ? hash.substr(1) : hash;
+        if (tag) {
+            setHashTag(tag);
+        }
+    }
+
+    function setHashTag (tag) {
+        $hashtag.text('');
+
+        var hashtag = '#' + tag;
+        hashtag.split('').forEach(function (symbol, i) {
+            var $a = $('<a/>')
+                .click(symbol, showSymbol)
+                .attr('href', '#')
+                .text(symbol)
+                .append('<sub>' + i + '</sub>');
+            $hashtag.append($a);
+        })
+        $container.bigtext();
+    }
+
+    function showSymbol (event) {
+        $hashtag.text('');
+        setSymbol(event.data);
+        $container.bigtext();
+        return false;
+    }
+
+    function setSymbol (symbol) {
+        $symbol.text(symbol);
+    }
+
+    hashchange();
+});
